109947ec9a744a9ce7d9812752298cd81942d30c
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2016-07-17 22:52:43 -0400

    Updating more meteor packages.  Removing oauath configuration because it will be unnecessary for this site.

--- server/modules/startup.js
+++ server/modules/startup.js
@@ -1,9 +1,7 @@
 let startup = () => {
   _setEnvironmentVariables();
-  _configureServices();  
 };
 
 let _setEnvironmentVariables = () => Modules.server.setEnvironmentVariables();
-let _configureServices = () => Modules.server.configureServices();
 
 Modules.server.startup = startup;
